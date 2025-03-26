dc3fc653622c03d2ccf2eb11d6348a73186c0a09
Igor C. Brasil <igorbrasilc@hotmail.com>
2025-03-26 13:57:08 -0300

    feat: adds threshold to test runner

--- packages/react/vite.config.js
+++ packages/react/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
+      lines: 80,
+      functions: 80,
+      branches: 70,
+      statements: 80,
     },
   },
 });
